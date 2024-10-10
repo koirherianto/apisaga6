6b21d5ce08fe40ce2ad3a961c2e3c6c1876c2ac9
koirherianto <koir.harianto@gmail.com>
2024-10-10 16:45:17 +0800

    pages docs rubah ke beberapa component

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,34 @@ export default {
     "./resources/**/*.{js,ts,jsx,tsx,vue}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        geist: ["Geist", "sans-serif"], // Gunakan font Geist untuk semua variasi
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "0rem",
+          sm: "2rem",
+          md: "0.5rem",
+          lg: "0.5rem", // Tidak ada padding untuk layar besar
+          xl: "0rem", // Tidak ada padding untuk layar ekstra besar
+          "2xl": "0rem",
+          "3xl": "0rem",
+
+          // Sesuaikan juga untuk xl jika diperlukan
+        },
+        screens: {
+          sm: "640px",
+          md: "768px",
+          lg: "1024px",
+          xl: "1280px",
+          "2xl": "1440px",
+          "3xl": "1940px",
+          // Tambahkan breakpoints untuk 1440px
+        },
+      },
+    },
   },
   plugins: [daisyui],
 }
