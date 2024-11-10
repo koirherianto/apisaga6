eb383f1c333490ec0d0f9262e9010569e5d56532
koirherianto <koir.harianto@gmail.com>
2024-11-10 17:30:12 +0800

    svelte code editor

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
+import { getDirname } from '@adonisjs/core/helpers'
 import adonisjs from '@adonisjs/vite/client'
 import inertia from '@adonisjs/inertia/client'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import sveltePreprocess from 'svelte-preprocess'
 
 export default defineConfig({
   plugins: [
@@ -17,8 +19,21 @@ export default defineConfig({
        */
       reload: ['resources/views/**/*.edge'],
     }),
+    svelte({
+      compilerOptions: { hydratable: true },
+      preprocess: sveltePreprocess({ typescript: true }),
+    }),
     inertia({ ssr: { enabled: true, entrypoint: 'inertia/app/ssr.ts' } }),
-    svelte({ compilerOptions: { hydratable: true } }),
-    adonisjs({ entrypoints: ['inertia/app/app.ts'], reload: ['resources/views/**/*.edge'] })
+    adonisjs({ entrypoints: ['inertia/app/app.ts'], reload: ['resources/views/**/*.edge'] }),
   ],
+
+  /**
+   * Define aliases for importing modules from
+   * your frontend code
+   */
+  resolve: {
+    alias: {
+      '~/': `${getDirname(import.meta.url)}/inertia/`,
+    },
+  },
 })
